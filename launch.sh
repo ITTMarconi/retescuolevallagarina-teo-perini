73427fdcffa4c68643e932b7eaa56d6a40ffdb9c
Teo Perini <21112949-teo.perini@users.noreply.gitlab.com>
2024-05-14 16:36:01 +0200

    Added launch script
    Former-commit-id: 89ab4b27b9ad742a8463cdb44d1b93aadd852e6e

---
+++ launch.sh
@@ -0,0 +1,35 @@
+# Set ip and port!!!
+vi Data/constants.js
+vi Data/constants.ts
+#####
+
+# Build and start server
+cd Server
+./server_run.sh
+
+# DO NOT STOP SERVER
+
+# check if the server is on
+curl http://<SERVER_IP>:<SERVER_PORT>/Data/Istituti/Arcivescovile/data.json
+
+cd ..
+
+# Build app
+cd App
+npm install
+npm run build
+
+cd ..
+
+
+# Kill server
+docker stop made-server_instance
+
+
+docker-compose up
+
+# # Test if server is up and running
+# curl http://<SERVER_IP>:<SERVER_PORT>/Data/Istituti/Arcivescovile/data.json
+
+# # Test if app it's running
+# curl http://<SERVER_IP>/
