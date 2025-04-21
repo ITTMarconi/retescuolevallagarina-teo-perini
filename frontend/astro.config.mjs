c413de60d124f3f109511b642e39735972a1028f
Mauro Longano <mauro.longano@marconirovereto.it>
2025-04-21 11:45:36 +0200

    feat: add environment configuration with API URL, PORT, and API_SECRET

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  env: {
+    schema: {
+      PUBLIC_API_URL: envField.string({ context: "client", access: "public", default: "https://api.retescuolevallagarina.it" }),
+      PORT: envField.number({ context: "server", access: "public", default: 4321 }),
+      API_SECRET: envField.string({ context: "server", access: "secret" }),
+    }
+  }
+
 
 });
