e9b4cf4465678c2a7dd0bb955629e5fffdb3c8e7
Mauro Longano <mauro.longano@marconirovereto.it>
2025-04-22 01:01:35 +0200

    build: add node driver for Astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,16 +1,30 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   env: {
     schema: {
-      PUBLIC_API_URL: envField.string({ context: "client", access: "public", default: "https://api.retescuolevallagarina.it" }),
-      PORT: envField.number({ context: "server", access: "public", default: 4321 }),
+      PUBLIC_API_URL: envField.string({
+        context: "client",
+        access: "public",
+        default: "https://api.retescuolevallagarina.it",
+      }),
+      PORT: envField.number({
+        context: "server",
+        access: "public",
+        default: 4321,
+      }),
       API_SECRET: envField.string({ context: "server", access: "secret" }),
-    }
-  }
-
-
+    },
+  },
+  server: {
+    allowedHosts: ["directus.retescuolevallagarina.it"],
+    host: "0.0.0.0", // Essential for containerized environments
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
